028ef5478c7c505478f48b19a30946256cd026b7
Mateusz Kawka <kaawkamateusz@gmail.com>
2018-10-06 22:35:52 +0200

    add unit tests for store actions

---
+++ tests/unit/store/actions.spec.js
@@ -0,0 +1,71 @@
+import actions from '@/store/actions.js';
+
+jest.useFakeTimers();
+
+// simulation local storage
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+};
+
+global.localStorage = new LocalStorageMock;
+
+var store = {
+  state: {
+    showInfo: true,
+    name: 'name',
+    tasks: [
+      {
+        title: 'task',
+        id: 1,
+        desc: 'task description',
+        completed: false
+      },
+      {
+        title: 'task 2',
+        id: 2,
+        desc: 'second task description',
+        completed: true
+      },
+      {
+        title: 'task 3',
+        id: 3,
+        desc: 'third task description',
+        completed: false
+      }
+    ]
+  }
+}
+
+test('actions--infoClose', () => {
+  actions.infoClose()
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+  expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 4000);
+})
+
+test('actions--saveTasksToLocalStorage', () => {
+  actions.saveTasksToLocalStorage(store)
+  expect(localStorage.tasks).toEqual(JSON.stringify(store.state.tasks))
+})
+
+test('actions--saveNameToLocalStorage', () => {
+  actions.saveNameToLocalStorage(store)
+  expect(localStorage.name).toEqual(JSON.stringify(store.state.name))
+})
